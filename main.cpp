4e00d388b0a393de230031bbced46adb83223825
Said Garcia <said.garcia12@gmail.com>
2017-03-26 22:27:37 -0500

    added comments to the funtions

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 
 using namespace std;
 
+//gets user input for use in divide()
 int getInput()
 {
     int in;
@@ -10,6 +11,9 @@ int getInput()
     return in;
 }
 
+//this divides and prints the results(to be added)
+
+//where the magic happens
 int main()
 {
     int dividen(getInput());
